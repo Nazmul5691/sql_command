fea3a3ddeb405d27192be69fa46054e7d9027a3d
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-09-21 21:56:17 +0600

    add delete commands

---
+++ delete.sql
@@ -0,0 +1,22 @@
+select * from students
+
+-- drop table students
+-- delete from students
+  
+select * from students where grade = 'C'
+  
+delete from students where grade = 'C'
+
+
+select * from students where age > 20 and grade = 'B+'
+
+delete from students where age > 20 and grade = 'B+'
+
+
+
+
+
+
+
+
+
