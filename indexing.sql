eae34c14e215ccb9fb0265ed607bc0c12d415012
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-09-21 21:59:12 +0600

    add indexing

---
+++ indexing.sql
@@ -0,0 +1,133 @@
+create table users2 (
+  id serial primary key,
+  name varchar(50),
+  email varchar(100),
+  country varchar(50)
+)
+
+
+
+-- Insert 100 sample rows
+INSERT INTO users2 (name, email, country) VALUES
+('Akash', 'akash1@example.com', 'Bangladesh'),
+('Rahim', 'rahim2@example.com', 'India'),
+('Karim', 'karim3@example.com', 'Nepal'),
+('Sajid', 'sajid4@example.com', 'Pakistan'),
+('Nazmul', 'nazmul5@example.com', 'Sri Lanka'),
+('Fahim', 'fahim6@example.com', 'Bangladesh'),
+('Rifat', 'rifat7@example.com', 'India'),
+('Tania', 'tania8@example.com', 'Nepal'),
+('Mim', 'mim9@example.com', 'Pakistan'),
+('Rony', 'rony10@example.com', 'Sri Lanka'),
+('Sumon', 'sumon11@example.com', 'Bangladesh'),
+('Tuhin', 'tuhin12@example.com', 'India'),
+('Jamil', 'jamil13@example.com', 'Nepal'),
+('Rakib', 'rakib14@example.com', 'Pakistan'),
+('Lamia', 'lamia15@example.com', 'Sri Lanka'),
+('Parvez', 'parvez16@example.com', 'Bangladesh'),
+('Shihab', 'shihab17@example.com', 'India'),
+('Omar', 'omar18@example.com', 'Nepal'),
+('Sadia', 'sadia19@example.com', 'Pakistan'),
+('Nadia', 'nadia20@example.com', 'Sri Lanka'),
+('Rasel', 'rasel21@example.com', 'Bangladesh'),
+('Kamal', 'kamal22@example.com', 'India'),
+('Jahid', 'jahid23@example.com', 'Nepal'),
+('Tareq', 'tareq24@example.com', 'Pakistan'),
+('Liton', 'liton25@example.com', 'Sri Lanka'),
+('Mehedi', 'mehedi26@example.com', 'Bangladesh'),
+('Shuvo', 'shuvo27@example.com', 'India'),
+('Arif', 'arif28@example.com', 'Nepal'),
+('Nafis', 'nafis29@example.com', 'Pakistan'),
+('Borna', 'borna30@example.com', 'Sri Lanka'),
+('Esha', 'esha31@example.com', 'Bangladesh'),
+('Hasib', 'hasib32@example.com', 'India'),
+('Rumi', 'rumi33@example.com', 'Nepal'),
+('Pavel', 'pavel34@example.com', 'Pakistan'),
+('Sumi', 'sumi35@example.com', 'Sri Lanka'),
+('Joya', 'joya36@example.com', 'Bangladesh'),
+('Arman', 'arman37@example.com', 'India'),
+('Farhan', 'farhan38@example.com', 'Nepal'),
+('Rini', 'rini39@example.com', 'Pakistan'),
+('Sajib', 'sajib40@example.com', 'Sri Lanka'),
+('Kabir', 'kabir41@example.com', 'Bangladesh'),
+('Asha', 'asha42@example.com', 'India'),
+('Bithi', 'bithi43@example.com', 'Nepal'),
+('Toma', 'toma44@example.com', 'Pakistan'),
+('Tanzim', 'tanzim45@example.com', 'Sri Lanka'),
+('Milon', 'milon46@example.com', 'Bangladesh'),
+('Jubair', 'jubair47@example.com', 'India'),
+('Saima', 'saima48@example.com', 'Nepal'),
+('Nusrat', 'nusrat49@example.com', 'Pakistan'),
+('Habib', 'habib50@example.com', 'Sri Lanka'),
+('Anik', 'anik51@example.com', 'Bangladesh'),
+('Rashed', 'rashed52@example.com', 'India'),
+('Rafsan', 'rafsan53@example.com', 'Nepal'),
+('Shakib', 'shakib54@example.com', 'Pakistan'),
+('Towhid', 'towhid55@example.com', 'Sri Lanka'),
+('Mahin', 'mahin56@example.com', 'Bangladesh'),
+('Asif', 'asif57@example.com', 'India'),
+('Maruf', 'maruf58@example.com', 'Nepal'),
+('Zihan', 'zihan59@example.com', 'Pakistan'),
+('Niloy', 'niloy60@example.com', 'Sri Lanka'),
+('Priya', 'priya61@example.com', 'Bangladesh'),
+('Mahi', 'mahi62@example.com', 'India'),
+('Shila', 'shila63@example.com', 'Nepal'),
+('Amin', 'amin64@example.com', 'Pakistan'),
+('Shuvo', 'shuvo65@example.com', 'Sri Lanka'),
+('Afif', 'afif66@example.com', 'Bangladesh'),
+('Nirob', 'nirob67@example.com', 'India'),
+('Sajjad', 'sajjad68@example.com', 'Nepal'),
+('Adnan', 'adnan69@example.com', 'Pakistan'),
+('Rana', 'rana70@example.com', 'Sri Lanka'),
+('Rafi', 'rafi71@example.com', 'Bangladesh'),
+('Tariq', 'tariq72@example.com', 'India'),
+('Hasan', 'hasan73@example.com', 'Nepal'),
+('Imran', 'imran74@example.com', 'Pakistan'),
+('Sabbir', 'sabbir75@example.com', 'Sri Lanka'),
+('Monir', 'monir76@example.com', 'Bangladesh'),
+('Sharif', 'sharif77@example.com', 'India'),
+('Ruhul', 'ruhul78@example.com', 'Nepal'),
+('Tanvir', 'tanvir79@example.com', 'Pakistan'),
+('Jibon', 'jibon80@example.com', 'Sri Lanka'),
+('Sohag', 'sohag81@example.com', 'Bangladesh'),
+('Maksud', 'maksud82@example.com', 'India'),
+('Bashir', 'bashir83@example.com', 'Nepal'),
+('Rubel', 'rubel84@example.com', 'Pakistan'),
+('Alim', 'alim85@example.com', 'Sri Lanka'),
+('Sabbir', 'sabbir86@example.com', 'Bangladesh'),
+('Arafat', 'arafat87@example.com', 'India'),
+('Ovi', 'ovi88@example.com', 'Nepal'),
+('Sajol', 'sajol89@example.com', 'Pakistan'),
+('Shawon', 'shawon90@example.com', 'Sri Lanka'),
+('Tanzila', 'tanzila91@example.com', 'Bangladesh'),
+('Raisa', 'raisa92@example.com', 'India'),
+('Sumaiya', 'sumaiya93@example.com', 'Nepal'),
+('Papia', 'papia94@example.com', 'Pakistan'),
+('Shamim', 'shamim95@example.com', 'Sri Lanka'),
+('Faria', 'faria96@example.com', 'Bangladesh'),
+('Nabila', 'nabila97@example.com', 'India'),
+('Rafiq', 'rafiq98@example.com', 'Nepal'),
+('Sami', 'sami99@example.com', 'Pakistan'),
+('Ayon', 'ayon100@example.com', 'Sri Lanka');
+
+explain analyze
+select * from users2 where email = 'towhid55@example.com'
+
+
+create index idx_users_email
+on users2(email)
+
+
+drop index idx_users_email
+
+explain analyze
+select * from users2 where email = 'towhid55@example.com'
+
+
+
+
+
+
+
+
+
